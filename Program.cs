ffe425684fa71babe65641110cc9452caa126726
Florian Gschwandtner <florian.gschwandtner@munich-airport.de>
2022-03-04 17:52:11 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var a = new ValueTuple<int[], int>[]
+{
+	new ValueTuple<int[], int>(new[] { 0, 1 }, 1),
+	new ValueTuple<int[], int>(new[] { 0, 1 }, 1)
+};
+
+for (var i1 = 0; i1 < a.Length; i1++)
+{
+	var length = a[i1].Item1.Length;
+	
+	for (var i2 = 0; i2 < length; i2++)
+	{
+		var elem = a[i1].Item1[i2];
+		
+		Console.WriteLine(elem);
+	}
+}
